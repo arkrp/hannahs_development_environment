9875dad180d2ea9ded28466424f6e8eb7081462e
Hannah Nelson <nexec64@gmail.com>
2026-07-10 01:37:49 -0700

    Turned the security down to a regular ssh container

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 project_directory=$(realpath $(dirname $BASH_SOURCE))
-podman build --jobs 0 -t ssh_forward_hop $project_directory
+podman build --jobs 0 -t secure_execution_sandbox $project_directory
 podman run \
    --replace \
-   -d \
-   --pod test_service_pod \
+   -it \
+   -p 9345:22 \
    -v $project_directory/ssh:/app/ssh \
-   --name ssh_forward_hop \
-   ssh_forward_hop
-#-p 9345:22 \
+   --name secure_execution_sandbox \
+   secure_execution_sandbox $1
+#--pod test_service_pod \
