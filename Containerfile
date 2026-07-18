c87e2e8bd1c9fd796f214d655dea29f642d166d8
Hannah Nelson <nexec64@gmail.com>
2026-07-17 23:09:09 -0700

    changed the user name

--- Containerfile
+++ Containerfile
@@ -3,9 +3,10 @@ WORKDIR app
 RUN apt-get update && apt-get -y install \
    ssh \
    sshfs
-RUN useradd -m accessuser
+RUN useradd -m ai_agent
 RUN groupadd -g 995 sshd
 RUN usermod -g 995 sshd
 COPY ./files/container_startup_script.sh /app/container_startup_script.sh
 COPY ./files/sshd_config /etc/ssh/sshd_config
+EXPOSE 22
 CMD ["/bin/bash", "/app/container_startup_script.sh"]
--- files/container_startup_script.sh
+++ files/container_startup_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "starting ssh tunnel hop"
-mkdir /home/accessuser/.ssh
-cp -R /run/secrets/ai_execution_sandbox_authorized_keys /home/accessuser/.ssh/authorized_keys
-cp -R /run/secrets/ai_execution_sandbox_ssh_private_key /home/accessuser/.ssh/id_rsa
-chown -R accessuser:accessuser /home/accessuser/.ssh
+echo "loading ssh keys"
+mkdir /home/ai_agent/.ssh
+cp -R /run/secrets/ai_execution_sandbox_authorized_keys /home/ai_agent/.ssh/authorized_keys
+cp -R /run/secrets/ai_execution_sandbox_ssh_private_key /home/ai_agent/.ssh/id_rsa
+chown -R ai_agent:ai_agent /home/ai_agent/.ssh
+echo "activating sshd"
 chmod 0755 /var/run/sshd
-echo "ssh proxy hop is online"
 /usr/sbin/sshd -D
