999af5c02c05138d88179468ebe72ef9a4b338f2
Hannah Nelson <nexec64@gmail.com>
2026-07-20 23:31:49 -0700

    remved vestigial from the dev startup script

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Hannah Nelson 2026
 TODO:
 Switch over to a sshfs on dev_container setup:
     Remove vesitigial traits (hostkeys, sandbox account, extraneous keys) from files
-        dev startup script
         dev containerfile
         sandbox startup script
         sandbox containerfile
--- dev_container/files/container_startup_script.sh
+++ dev_container/files/container_startup_script.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 echo "loading ssh keys"
-#section-start specify host key
-cp -R /run/secrets/dev_host_key /etc/ssh/dev_host_key
-chmod 600 /etc/ssh/dev_host_key
-#section-end
 #section-start load dev account keys
 mkdir /home/dev/.ssh
 cp -R /run/secrets/admin_key.pub /home/dev/.ssh/authorized_keys
@@ -11,12 +7,6 @@ cp -R /run/secrets/dev_key /home/dev/.ssh/id_rsa
 chown -R dev:dev /home/dev/.ssh
 chmod 644 /home/dev/.ssh/authorized_keys
 chmod 600 /home/dev/.ssh/id_rsa
-#section-end
-#section-start load sandbox account keys
-mkdir /home/sandbox/.ssh
-cp -R /run/secrets/sandbox_key.pub /home/sandbox/.ssh/authorized_keys
-chown -R sandbox:sandbox /home/sandbox/.ssh
-chmod 644 /home/sandbox/.ssh/authorized_keys
 #section-end
 echo "activating sshd"
 chmod 0755 /var/run/sshd
