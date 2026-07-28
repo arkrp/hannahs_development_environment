f78430ae322c9543fba1b6d2b59f8423d7e77b1b
Hannah Nelson <nexec64@gmail.com>
2026-07-28 00:23:35 -0700

    made the sanbox command intepret the commands send from bash rather than raw

--- dev_container/files/sandbox.sh
+++ dev_container/files/sandbox.sh
@@ -1,5 +1,5 @@
 if [[ $(pwd) == "/home/dev/workspace/"* ]] || [ $(pwd) == "/home/dev/workspace" ]; then
-   ssh -t dev@sandbox "cd $(pwd); $@"
+   ssh -t dev@sandbox "cd $(pwd); bash $@"
 else
    echo "sandbox only works inside the /home/dev/workspace directory."
    exit 1
